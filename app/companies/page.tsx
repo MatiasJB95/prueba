74818e3231276418545d12df06c5d9873393b3ab
Matias Badano <115195236+MatiasJB95@users.noreply.github.com>
2025-08-26 14:34:30 -0300

    Udapte

--- app/companies/page.tsx
+++ app/companies/page.tsx
@@ -1,6 +1,6 @@
 import { CompaniesHero } from "@/components/companies/companies-hero"
-import { Header } from "@/components/header"
-import { Footer } from "@/components/footer"
+import  Header  from "@/components/header"
+import  Footer  from "@/components/footer"
 
 export default function CompaniesPage() {
   return (
